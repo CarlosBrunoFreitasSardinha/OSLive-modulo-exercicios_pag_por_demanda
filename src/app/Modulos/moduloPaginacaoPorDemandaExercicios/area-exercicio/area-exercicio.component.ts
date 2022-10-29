c072b69152d576195882aba2389913b58377d213
carlosifto <carlosifto@gmail.com>
2022-10-29 12:36:56 -0300

    correcoes modals

--- src/app/Modulos/moduloPaginacaoPorDemandaExercicios/area-exercicio/area-exercicio.component.ts
+++ src/app/Modulos/moduloPaginacaoPorDemandaExercicios/area-exercicio/area-exercicio.component.ts
@@ -42,9 +42,6 @@ export class AreaExercicioComponent implements OnInit, OnChanges{
   
   ngOnChanges(changes: SimpleChanges): void {
     this.preencherMemoriaFisica();
-    console.log("CHANGEI"+this.exercicioSelecionado)
-    console.log(this.respostaMemoriaLogica)
-    console.log(this.listaProcessos)
   }
 
   preencherMemoriaFisica(){
